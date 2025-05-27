dd5ce8c0e6680f9418e356b03f3b7a39c0766856
Ivankirev39 <ivakir01@easv365.dk>
2025-05-27 13:21:06 +0300

    Update page-treatments-selector.php

--- template-parts/page-treatments-selector.php
+++ template-parts/page-treatments-selector.php
@@ -9,13 +9,13 @@ $loop = new WP_Query($args);
 <?php if($loop->have_posts()): ?>
   <div class="w-full bg-[#D9D9D9]/40 py-8 px-4">
     <div class="w-full text-center mt-5 mb-10">
-      <h2>HCancer Treatments</h2>
+      <h2>Cancer Treatments</h2>
     </div>
     <h3 class="text-center text-[#2B682C] mb-6 md:mb-8 lg:mb-10">Choose a category</h3>
     <div class="tab-container flex flex-wrap justify-center gap-4 w-full">
       <?php while($loop->have_posts()): $loop->the_post(); ?>
         <?php $treatmentTab = get_field("treatment_tab"); ?>
-        <button class="bg-[#580259] text-[#ededed] rounded-xl px-6 py-3 uppercase text-center cursor-pointer">
+        <button class="bg-[#580259] text-[#ededed] rounded-xl px-6 py-3 uppercase text-center cursor-pointer" key="1">
           <?php echo esc_html($treatmentTab) ?>
         </button>
       <?php endwhile; ?>
