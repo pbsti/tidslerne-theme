2b68fb7d3df8b08b8192bc7c64e016bfb42cc2ce
Ivankirev39 <ivakir01@easv365.dk>
2025-05-27 13:17:05 +0300

    javascript link

--- functions.php
+++ functions.php
@@ -4,3 +4,14 @@ function thistle_load_resources() {
     wp_enqueue_style("styles", get_template_directory_uri() . "/css/styles.css");
 }
 add_action("wp_enqueue_scripts", "thistle_load_resources");
+
+function enqueue_main_js() {
+    wp_enqueue_script(
+        'main-js', 
+        get_template_directory_uri() . '/js/main.js', 
+        array(), 
+        null, 
+        true 
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_main_js');
---
+++ js/main.js
@@ -0,0 +1,51 @@
+  // Mobile menu toggle
+  document.getElementById('mobileMenuBtn').onclick = function() {
+    document.getElementById('mobileNavMenu').classList.remove('hidden');
+  };
+  document.getElementById('closeMobileMenu').onclick = function() {
+    document.getElementById('mobileNavMenu').classList.add('hidden');
+  };
+
+  // Hide "Frontpage" link on index.html only
+  if (window.location.pathname.endsWith('index.html') || window.location.pathname === '/' ) {
+    var frontpageLink = document.getElementById('frontpage-link');
+    if(frontpageLink) frontpageLink.style.display = 'none';
+    var frontpageLinkMobile = document.getElementById('frontpage-link-mobile');
+    if(frontpageLinkMobile) frontpageLinkMobile.style.display = 'none';
+  }
+
+
+
+
+// Tabs
+
+
+
+    const tabs = document.querySelectorAll(".tab-container button");
+    const panels = document.getElementsByClassName("panel");
+
+    tabs.forEach((tab) => tab.addEventListener("click", handleTabClick));
+
+    function handleTabClick(e) {
+      const targetKey = e.target.getAttribute("key");
+      
+      tabs.forEach((tab) => {
+        tab.classList.remove("bg-[#580259]");
+        tab.classList.add("bg-[#2B682C]");
+      });
+      
+      document.querySelectorAll(`.tab-container button[key="${targetKey}"]`).forEach(tab => {
+        tab.classList.remove("bg-[#2B682C]");
+        tab.classList.add("bg-[#580259]");
+      });
+      
+      for (const panel of panels) {
+        const key = panel.getAttribute("key");
+        if (key === targetKey) {
+          panel.classList.remove("hidden");
+        } else {
+          panel.classList.add("hidden");
+        }
+      }
+    }
+
